bbf083ccd8d601497cfe7a41749a1f7c07a5a313
engalar <engalar@gmail.com>
2022-04-14 18:02:52 +0800

    wip

--- src/Amap.tsx
+++ src/Amap.tsx
@@ -1,7 +1,9 @@
-import { createElement, ReactElement } from "react";
+import { createElement, ReactElement, useEffect } from "react";
 import { AmapProps } from "../typings/AmapProps";
 import { AmapStyle } from "./ui/Styles";
 import { MapView, MapType } from "react-native-amap3d";
+import { AMapSdk } from "react-native-amap3d";
+import { Platform } from 'react-native';
 
 export function Amap(props: AmapProps<AmapStyle>): ReactElement {
     // const styles = flattenStyles(defaultAmapStyle, props.style);
@@ -14,6 +16,19 @@ export function Amap(props: AmapProps<AmapStyle>): ReactElement {
     //     );
     // }, [props.caption, styles]);
     console.log(props);
+
+    useEffect(() => {
+        AMapSdk.init(
+            Platform.select({
+                android: "c52c7169e6df23490e3114330098aaac",
+                ios: "186d3464209b74effa4d8391f441f14d",
+            })
+        );
+
+        return () => {
+        }
+    }, [])
+
 
     return (
         <MapView
